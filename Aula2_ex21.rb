adb92c15af6c85859d09813a7897ffef2d2daf23
Rogerio Augusto <raa.augusto@hotmail.com>
2023-07-11 22:43:24 -0300

    Ex23 aula 11/07

--- Aula2_ex21.rb
+++ Aula2_ex21.rb
@@ -4,3 +4,49 @@ informado na idade é do tipo Integer, caso sim, prossiga, caso não, encerre o 
 seguida, exiba uma mensagem de boas-vindas que inclua o nome do usuário e seu grupo
 etário, utilizando condicionais if-elsif-else.
 =end
+
+print "Insira seu nome: "
+nome = gets.chomp
+
+print "Insira sua idade: "
+idade = gets.chomp.to_i
+
+
+if idade.is_a?(Integer) 
+    if idade < 18 
+        puts "Bem-vindo, #{nome}, você é uma criança"
+    elsif idade >=18 && idade < 65
+        puts "Bem-vindo, #{nome}, você é um adulto"
+    else
+        puts "Bem-vindo, #{nome}, você é um idoso" 
+    end
+#else
+#   puts "Programa encerrado"
+end
+
+
+#Feito pelo Chat GPT
+=begin
+print "Insira seu nome: "
+nome = gets.chomp
+
+print "Insira sua idade: "
+idade = gets.chomp
+
+if idade =~ /\A\d+\z/
+  idade = idade.to_i
+  
+  if idade < 18
+    puts "Bem-vindo, #{nome}, você é uma criança"
+  elsif idade >= 18 && idade < 65
+    puts "Bem-vindo, #{nome}, você é um adulto"
+  else
+    puts "Bem-vindo, #{nome}, você é um idoso"
+  end
+else
+  puts "Programa encerrado"
+end
+=end
+
+    
+
