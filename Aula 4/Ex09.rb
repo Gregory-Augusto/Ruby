3d76df87f08896ffe0e4b1dd263045ce5ca3bb3d
Rogerio Augusto <raa.augusto@hotmail.com>
2023-07-14 19:24:28 -0300

    correção de exercício

--- Aula 4/Ex09.rb
+++ Aula 4/Ex09.rb
@@ -6,9 +6,8 @@ números pares da lista.
 def lista_numeros(*numeros)
     pares = numeros.select { |numero| numero.even? }  # Filtra os números pares
     media = pares.sum.to_f / pares.length  # Calcula a média dos números pares
-  
     puts "A média dos números pares será: #{media}"
-  end
+end
   
   lista_numeros(1, 2, 3, 4, 5, 6)
   
