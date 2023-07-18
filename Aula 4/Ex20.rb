e38f6ae410139eab33de7bb83c2050f344783233
Rogerio Augusto <raa.augusto@hotmail.com>
2023-07-17 21:25:46 -0300

    Desafio da Aula 4

--- Aula 4/Ex20.rb
+++ Aula 4/Ex20.rb
@@ -4,21 +4,28 @@ Crie um método chamado "prime_numbers" que receba um número inteiro como parâ
 =end
 
 def prime_numbers(valor)
-    primes = []
-    
-    for numero in 2...valor
-      if (2..Math.sqrt(numero)).none? { |i| numero % i == 0 }
-        primes << numero
-      elsif (2..Math.sqrt(numero)).none? { |i| numero % i == valor }
-        primes << numero
-      end
-    end
-
-    return primes
-end
-          
-print prime_numbers(17)
+  primes = []
   
+  for numero in 2..valor
+    if (2..Math.sqrt(numero)).none? { |i| numero % i == 0 }
+      primes << numero
+    end
+  end
+
+  return primes
+end
+
+# Solicitar o valor ao usuário
+print "Digite um valor: "
+valor = gets.chomp.to_i
+
+# Chamada da função e armazenamento dos números primos
+primos = prime_numbers(valor)
+
+# Imprimir os números primos
+puts "Números primos menores ou iguais a #{valor}:"
+puts primos.inspect
+
         
 
   
