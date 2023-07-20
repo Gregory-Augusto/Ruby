997b64a4fbc3bfa0a3a97946225510c4f8407f2e
Rogerio Augusto <raa.augusto@hotmail.com>
2023-07-20 18:59:38 -0300

    Atualizando o desafio da aula 8

--- Aula 8/Desafio.rb
+++ Aula 8/Desafio.rb
@@ -1,6 +1,11 @@
 invalid_number = '981289312988712783'
 valid_number   = '(19) 99999-9999'
 
+
+# Outras possibilidades de expressões regulares:
+# ^\([1-9]{2}\)[1-9]{5}\-[1-9]{4}
+# ^\([1-9]{2}\)\ ?[1-9](?:\ ?\d){4}-\d{4}$
+
 def valid_phone_number?(phone_number)
   /^\([1-9]{2}\) (9[1-9])[0-9]{3}\-[0-9]{4}$/.match?(phone_number)
 end
