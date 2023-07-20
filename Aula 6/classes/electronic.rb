2c58f18204994c57b9ccc444d773b991b918f3d0
Rogerio Augusto <raa.augusto@hotmail.com>
2023-07-20 17:11:23 -0300

    Arrumando o código que uso de base na Kazap

--- Aula 6/classes/electronic.rb
+++ Aula 6/classes/electronic.rb
@@ -1,47 +1,40 @@
-#sublasse electronic
-
 class Electronic < Product
-    attr_accessor :brand, :model, :power
+	attr_accessor :brand, :model, :power
 
-    def initialize(attributes = {})
-        super
-        @brand = attributes[:brand]
-        @model = attributes[:model]
-        @power = attributes[:power]
-    end
+	def initialize(attributes = {})
+		super
+		@brand = attributes[:brand]
+		@model = attributes[:model]
+		@power = false
+	end
 
-    def display
-        super
-        puts "Marca desconhecida." unless brand
-        puts "Marca: #{brand}" if brand
-        puts "Modelo: #{model}" if model
-    end
+	def display
+		super
+		puts "Marca: #{brand}" if brand
+		puts "Modelo: #{model}" if model
+	end
 
-    def turn_on 
-        if @power 
-            puts "O aparelho já está ligado"
-        else
-            @power = true
-            puts "Aparelho ligado."
-        end
-    end
+	def turn_on
+		if power
+			puts "O aparelho já está ligado."
+		else
+			@power = true
+			puts "Aparelho ligado."
+		end
+	end
 
-    def turn_off
-        if @power
-        @power = false
-        puts "Aparelho desligado."
-        else
-            puts "O aparelho já está ligado"
-        end
-    end
+	def turn_off
+		if power
+			@power = false
+			puts "Aparelho desligado."
+		else
+			puts "O aparelho já está desligado."
+		end
+	end
 
-    def turn_on_security_mode
-        turn_on
-        puts "Em modo de segurança"
-    end
-
-    def start_sale
-        puts "Produto de valor alto, atenção."
-    end
+	def turn_on_security_mode
+		turn_on
+		puts "Em modo de segurança."
+	end
 end
 
--- Aula 6/sandbox.rb
+++ Aula 6/sandbox.rb
@@ -1,95 +1,27 @@
-#área de testes
-require './modules/adressable'
-require './modules/validatable'
-require './classes/customer'
 require './modules/shipping'
 require './modules/payment'
+require './modules/validatable'
+require './modules/addressable'
 require './classes/product'
 require './classes/book'
 require './classes/electronic'
+require './classes/customer'
 
-
-# Aula 6
-
-#product = Product.new(name: "Xbox Series S", price: 2500)
-#product.display
-=begin
-book = Book.new(
-    name: "Assassin's Creed: Renascença",
-     price: 30,
-      author: "Jack kirby",
-       pages: 389
-    )
-book.display
-puts "-"
-book.start_sale
-book.display
-puts "-"
-book.stop_sale
-book.display
-book.read
-book.open(800)
-book.display
-=end
-
-=begin
-electronic = Electronic.new(
-    name: "Switch",
-    price: 2500,
-    brand: "Nintendo",
-    model: "859-F"
-)
-
-electronic.display
-puts
-electronic.on_sale
-puts
-electronic.stop_sale
-puts
-electronic.start_sale
-
-electronic.turn_on_security_mode
-electronic.turn_off
-electronic.turn_off
-electronic.turn_on
-electronic.turn_on
-=end
-
-# Aula 7
-=begin 
-
-book = Book.new(name: 'Livro teste', price: 20)
-
-card = {
-  card_number: '4444 4444 4444 4444',
-  card_cvv: '123',
-  card_holder: 'John Doe',
-  card_expiry: '2023-10-10'
+address = {
+  public_area: 'Dorival Bueno',
+  number: '110',
+  district: 'Morro do Ouro',
+  zipcode: '13840-064',
+  city: 'Mogi Guaçu',
+  federal_state: 'SP'
 }
-
-bank_slip = {
-  name: "Fred Flinston",
-  document: "123.354.322.232-10",
-  email: 'fredflinston@gmail.com'
-}
-
-book.pay({
-  method: 'bank_slip',
-  amount: 0,
-  options: bank_slip  # Passa o hash `bank_slip` como valor para a chave `options`
-})
-=end
-
-# Aula 8
 
 customer = Customer.new({
-  first_name: 'John',
-  last_name: "Constantine",
-  document: "123.456.789-10",
-  email:"john@constantine.com",
-  address: address
+  first_name: 'john',
+  last_name: 'appleseed',
+  document: '123.456.789-10',
+  email: 'john@appleseed.com',
+  address:
 })
 
 customer.show
-
-
